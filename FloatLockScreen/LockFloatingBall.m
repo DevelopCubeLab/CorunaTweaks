5e4a4ee898c5595b3ab0dad6d9e491f193b64779
DevelopLab <admin@developlab.com>
2026-03-20 10:56:19 +0800

    Add FloatLockScreen Tweak

---
+++ FloatLockScreen/LockFloatingBall.m
@@ -0,0 +1,114 @@
+#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
+
+@interface FBPassThroughWindow : UIWindow
+@end
+
+@implementation FBPassThroughWindow
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    UIView *hit = [super hitTest:point withEvent:event];
+    // 如果点到的是 window 自己或 rootView，则让事件穿透
+    if (hit == self || hit == self.rootViewController.view) {
+        return nil;
+    }
+    return hit;
+}
+@end
+
+static UIWindow *gWindow;
+static UIButton *gButton;
+
+/// 锁屏核心
+static void FBLockDevice(void) {
+    id sb = (id)[objc_getClass("SpringBoard") sharedApplication];
+    SEL sel = NSSelectorFromString(@"_simulateLockButtonPress");
+    if ([sb respondsToSelector:sel]) {
+        ((void (*)(id, SEL))objc_msgSend)(sb, sel);
+    }
+}
+
+@interface FBDragView : UIView
+@end
+
+@implementation FBDragView {
+    CGPoint start;
+}
+
+/// 创建一个Float View来用来触发锁屏
+- (instancetype)init {
+    self = [super initWithFrame:CGRectMake(50, 200, 44, 44)];
+    self.backgroundColor = [UIColor colorWithWhite:0 alpha:0.5];
+    self.layer.cornerRadius = 22;
+    
+    UILabel *l = [[UILabel alloc] initWithFrame:self.bounds];
+    l.text = @"🔒";
+    l.font = [UIFont systemFontOfSize:22];
+    l.textAlignment = NSTextAlignmentCenter;
+    [self addSubview:l];
+    
+    UIPanGestureRecognizer *pan =
+    [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
+    [self addGestureRecognizer:pan];
+    
+    UITapGestureRecognizer *tap =
+    [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap)];
+    [self addGestureRecognizer:tap];
+    
+    return self;
+}
+
+- (void)pan:(UIPanGestureRecognizer *)g {
+    CGPoint t = [g translationInView:self.superview];
+    self.center = CGPointMake(self.center.x + t.x, self.center.y + t.y);
+    [g setTranslation:CGPointZero inView:self.superview];
+}
+
+- (void)tap {
+    FBLockDevice();
+}
+
+@end
+
+static void setupFloatingBall(void) {
+
+    UIScene *targetScene = nil;
+    for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+        if (scene.activationState == UISceneActivationStateForegroundActive) {
+            targetScene = scene;
+            break;
+        }
+    }
+
+    if (targetScene && [targetScene isKindOfClass:[UIWindowScene class]]) {
+        gWindow = [[FBPassThroughWindow alloc] initWithWindowScene:(UIWindowScene *)targetScene];
+    } else {
+        gWindow = [[FBPassThroughWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    }
+
+    gWindow.frame = [UIScreen mainScreen].bounds;
+    gWindow.windowLevel = 2600;
+    gWindow.backgroundColor = UIColor.clearColor;
+
+    UIViewController *root = [UIViewController new];
+    root.view.backgroundColor = UIColor.clearColor;
+    gWindow.rootViewController = root;
+
+    FBDragView *v = [FBDragView new];
+    [root.view addSubview:v];
+
+    gWindow.hidden = NO;
+    [gWindow makeKeyAndVisible];
+}
+
+__attribute__((constructor))
+static void init(void) {
+
+    NSString *bid = [[NSBundle mainBundle] bundleIdentifier];
+    if (![bid isEqualToString:@"com.apple.springboard"]) return;
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2*NSEC_PER_SEC),
+                   dispatch_get_main_queue(), ^{
+        setupFloatingBall();
+    });
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # CorunaTweaks
-Some tweaks for Coruna
+Some tweaks for Coruna exploit chain (iOS 13.0 - 17.2.1).
+
+
+
+## Build
+
+1. FloatLockScreen
+`xcrun --sdk iphoneos clang -target arm64e-apple-ios14.0 -fobjc-arc -dynamiclib \
+    -o LockFloatingBall.dylib LockFloatingBall.m \
+    -framework Foundation -framework UIKit -framework QuartzCore -lobjc \
+    -Wl,-dead_strip -Os -undefined dynamic_lookup && \
+codesign -s - LockFloatingBall.dylib`
+
+
+
+## Reference
+
+1. [coruna](https://github.com/khanhduytran0/coruna)
+2. [TweaksLoader](https://github.com/AldazActivator/TweaksLoader)
+
